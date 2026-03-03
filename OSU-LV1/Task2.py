b217e1ad2bddf1426fdee08d7f1be4830fa00baf
Josip Mujan <mujan.josip1@gmail.com>
2026-03-03 11:40:08 +0100

    Add files via upload
    Napravljeno na labosima

---
+++ OSU-LV1/Task2.py
@@ -0,0 +1,23 @@
+while True:
+    x = input()
+    try:
+        value = float(x)
+    except:
+        print("Invalid input, please enter a numeric value")
+        continue
+
+    if not(0.0 <= value <= 1.0):
+        print("Number must be between 0.0 and 1.0")
+        continue
+
+    if value >= 0.9:
+        print("A")
+    elif value >= 0.8:
+        print("B")
+    elif value >= 0.7:
+        print("C")
+    elif value >= 0.6:
+        print("D")
+    else:
+        print("F")
+    break
